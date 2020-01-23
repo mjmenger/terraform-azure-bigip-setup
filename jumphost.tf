b691e02d7ec036d586936c6a0fd073bbc7b85a8c
Mark Menger <m.menger@f5.com>
2020-01-23 10:09:18 -0500

    enable the inventory file

--- jumphost.tf
+++ jumphost.tf
@@ -116,38 +116,38 @@ resource "azurerm_public_ip" "jh_public_ip" {
 #
 # Create and place the inventory.yml file for the ansible demo
 #
-# resource "null_resource" "transfer" {
-#     count = length(var.azs)
-#     provisioner "file" {
-#         content     = templatefile(
-#             "${path.module}/hostvars_template.yml",
-#                 {
-#                     bigip_host_ip          = azurerm_network_interface.mgmt-nic[count.index].private_ip_address#bigip_host_ip = module.bigip.mgmt_public_ips[count.index]  the ip address that the bigip has on the management subnet
-#                     bigip_host_dns         = azurerm_network_interface.ext-nic[count.index].private_ip_address # the DNS name of the bigip on the public subnet
-#                     bigip_domain           = "${var.region}.compute.internal"
-#                     bigip_username         = "admin"
-#                     bigip_password         = random_password.password.result
-#                     ec2_key_name           = basename(var.privatekeyfile) 
-#                     ec2_username           = "azureuser"
-#                     log_pool               = cidrhost(cidrsubnet(var.cidr,8,count.index + 30),250)
-#                     bigip_external_self_ip = azurerm_network_interface.ext-nic[count.index].private_ip_address # the ip address that the bigip has on the public subnet
-#                     bigip_internal_self_ip = azurerm_network_interface.int-nic[count.index].private_ip_address # the ip address that the bigip has on the private subnet
-#                     juiceshop_virtual_ip   = azurerm_network_interface.ext-nic[count.index].private_ip_addresses[1]
-#                     grafana_virtual_ip     = azurerm_network_interface.ext-nic[count.index].private_ip_addresses[2]
-#                     appserver_gateway_ip   = cidrhost(cidrsubnet(var.cidr,8,count.index + 30),1)
-#                     appserver_guest_ip     = azurerm_network_interface.app_nic[count.index].private_ip_address
-#                     appserver_host_ip      = azurerm_network_interface.jh_nic[count.index].private_ip_address   # the ip address that the jumphost has on the public subnet
-#                     bigip_dns_server       = "8.8.8.8"
-#                 }
-#         )
+resource "null_resource" "transfer" {
+    count = length(var.azs)
+    provisioner "file" {
+        content     = templatefile(
+            "${path.module}/hostvars_template.yml",
+                {
+                    bigip_host_ip          = azurerm_network_interface.mgmt-nic[count.index].private_ip_address#bigip_host_ip = module.bigip.mgmt_public_ips[count.index]  the ip address that the bigip has on the management subnet
+                    bigip_host_dns         = azurerm_network_interface.ext-nic[count.index].private_ip_address # the DNS name of the bigip on the public subnet
+                    bigip_domain           = "${var.region}.compute.internal"
+                    bigip_username         = "admin"
+                    bigip_password         = random_password.password.result
+                    ec2_key_name           = basename(var.privatekeyfile) 
+                    ec2_username           = "azureuser"
+                    log_pool               = cidrhost(cidrsubnet(var.cidr,8,count.index + 30),250)
+                    bigip_external_self_ip = azurerm_network_interface.ext-nic[count.index].private_ip_address # the ip address that the bigip has on the public subnet
+                    bigip_internal_self_ip = azurerm_network_interface.int-nic[count.index].private_ip_address # the ip address that the bigip has on the private subnet
+                    juiceshop_virtual_ip   = azurerm_network_interface.ext-nic[count.index].private_ip_addresses[1]
+                    grafana_virtual_ip     = azurerm_network_interface.ext-nic[count.index].private_ip_addresses[2]
+                    appserver_gateway_ip   = cidrhost(cidrsubnet(var.cidr,8,count.index + 30),1)
+                    appserver_guest_ip     = azurerm_network_interface.app_nic[count.index].private_ip_address
+                    appserver_host_ip      = azurerm_network_interface.jh_nic[count.index].private_ip_address   # the ip address that the jumphost has on the public subnet
+                    bigip_dns_server       = "8.8.8.8"
+                }
+        )
 
-#         destination = "~/inventory.yml"
+        destination = "~/inventory.yml"
 
-#         connection {
-#             type        = "ssh"
-#             user        = "azureuser"
-#             private_key = var.privatekeyfile
-#             host        = azurerm_public_ip.jh_public_ip[count.index].ip_address
-#         }
-#     }
-# }
+        connection {
+            type        = "ssh"
+            user        = "azureuser"
+            private_key = file(var.privatekeyfile)
+            host        = azurerm_public_ip.jh_public_ip[count.index].ip_address
+        }
+    }
+}
